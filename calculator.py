12ea460e66ad7c9c0d982c5bf3a628ceabb06928
Rabbit, Bae <cjfals0904@gmail.com>
2019-05-20 21:32:14 +0900

    update logic

--- calculator.py
+++ calculator.py
@@ -15,19 +15,23 @@ class NumberCnt:
     return self.cnt
 
 file_name = 'data.csv'
+result_file_name = 'result.csv'
 unique_numbers = []
 number_cnt_array = []
 d = open(file_name, 'r')
+result = open(result_file_name, 'r')
 data, labels = util.loadData(d)
+resultData, resultLables = util.loadResultData(result)
 data = np.int_(data)
+resultData = np.int_(resultData)
 for i in range(46):
   number_cnt_array.append(NumberCnt(i, 0))
 
 
-for x in range(len(data)):
+for x in range(len(resultData)):
     for y in range(len(data[x])):
-      numbersCnt = number_cnt_array[data[x][y]].getCnt()
-      number_cnt_array[data[x][y]].updateCnt(numbersCnt + 1)
+      numbersCnt = number_cnt_array[resultData[x][y]].getCnt()
+      number_cnt_array[resultData[x][y]].updateCnt(numbersCnt + 1)
       if data[x][y] not in unique_numbers:
         unique_numbers.append(data[x][y])
 
@@ -43,5 +47,25 @@ for i in range(5):
 # for j in range(len(number_cnt_array)):
 #   print("number %d's CNT: %d" % (j,number_cnt_array[j].getCnt()))
 # ran_numbers = random.sample(unique_numbers, k=6)
-
+max, max_numbers = util.getMaxCnt(number_cnt_array)
+min, min_numbers = util.getMinCnt(number_cnt_array)
+exception_array = util.exceptionMaxMinData(number_cnt_array, max, min)
+print(exception_array)
+print("historical data combination")
+for k in range(5):
+  print(sorted(random.sample(exception_array, k=6)))
 # print(ran_numbers)
+period = 0
+# 
+while True:
+  # tmp = sorted(random.sample(exception_array, k=6))
+  # tmp = sorted(random.sample(unique_numbers, k=6))
+  tmp = sorted(random.sample(range(1,45), 6))
+  if np.array_equal(tmp, [8,22,35,38,39,41]) is True:
+    print("lucky first!" + "{:,}".format(period))
+    break
+  elif period > 10000000:
+    print('oh my god!')
+    break
+  else:
+    period+=1
--- util.py
+++ util.py
@@ -9,3 +9,46 @@ def loadData(f_name):
         data[i] = data[i][0:len(data[i])-1]
 
     return data[:-2],label[2:]  #Removing first two and last two so each X[i] tries to predict Y[i+2] (i've used i+2 and not to i+1 to force it to predict the future (O) )
+def loadResultData(f_name):
+    data  = f_name.read().split("\n")
+    data = data[:len(data)-1]
+    label = []
+    for i in range(len(data)):
+        data[i] = data[i].split(",")
+        data[i] = [float(x) for x in data[i]]
+        label.append(data[i][len(data[i])-1])
+        data[i] = data[i][0:len(data[i])-1]
+
+    return data[:-2],label[2:]  #Removing first two and last two so each X[i] tries to predict Y[i+2] (i've used i+2 and not to i+1 to force it to predict the future (O) )
+def getMaxCnt(array):
+    max = 0
+    max_numbers = []
+    for i in range(len(array)):
+        max =  array[i].getCnt() if max < array[i].getCnt() else max
+    for j in range(len(array)):
+        if array[j].getCnt() == max:
+            max_numbers.append(array[j].number)
+    
+    return max, max_numbers
+def getMinCnt(array):
+    cnt_array = []
+    min_numbers = []
+    for i in range(len(array)):
+        if array[i].number != 0:
+            cnt_array.append(array[i].getCnt())
+
+    cnt_array = sorted(cnt_array)
+    min = cnt_array[0]
+    for j in range(len(array)):
+        if array[j].number == 0:
+            array[j].updateCnt(min)
+        else:
+            if array[j].getCnt() == min:
+                min_numbers.append(array[j].number)
+    return min, min_numbers
+def exceptionMaxMinData(array, max, min):
+    exception_array = []
+    for i in range(len(array)):
+        if array[i].getCnt() > min and array[i].getCnt() < max:
+            exception_array.append(array[i].number)
+    return exception_array
