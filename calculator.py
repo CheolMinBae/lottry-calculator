89e7765347551c32e01b7de1c3c2557d72d2a826
Rabbit, Bae <cjfals0904@gmail.com>
2019-05-20 20:22:28 +0900

    cnt 계산할 수 있도록 수정

--- calculator.py
+++ calculator.py
@@ -1,19 +1,41 @@
 import util
 import random
+import numpy as np
 # 자동 뽑은 것들 중에 풀랜덤 5개
 # 10회 당첨 내용 중에 안나온 것, 많이 나온 것 조합
+class NumberCnt:
+  def __init__(self, number, cnt):
+    self.number = number
+    self.cnt = cnt
+
+  def updateCnt (self, cnt):
+    self.cnt = cnt
+
+  def getCnt (self):
+    return self.cnt
+
 file_name = 'data.csv'
 unique_numbers = []
+number_cnt_array = []
 d = open(file_name, 'r')
 data, labels = util.loadData(d)
-print(data)
+data = np.int_(data)
+for i in range(46):
+  number_cnt_array.append(NumberCnt(i, 0))
+
+
 for x in range(len(data)):
     for y in range(len(data[x])):
+      numbersCnt = number_cnt_array[data[x][y]].getCnt()
+      number_cnt_array[data[x][y]].updateCnt(numbersCnt + 1)
       if data[x][y] not in unique_numbers:
         unique_numbers.append(data[x][y])
 for z in range(5):
   ran_numbers = random.sample(unique_numbers, k=6)
   print(ran_numbers)
+
+for j in range(len(number_cnt_array)):
+  print("number %d's CNT: %d" % (j,number_cnt_array[j].getCnt()))
 # ran_numbers = random.sample(unique_numbers, k=6)
 
 # print(ran_numbers)
--- util.py
+++ util.py
@@ -1,6 +1,5 @@
 def loadData(f_name):
     data  = f_name.read().split("\n")
-    print(data)
     data = data[:len(data)-1]
     label = []
     for i in range(len(data)):
@@ -8,4 +7,5 @@ def loadData(f_name):
         data[i] = [float(x) for x in data[i]]
         label.append(data[i][len(data[i])-1])
         data[i] = data[i][0:len(data[i])-1]
+
     return data[:-2],label[2:]  #Removing first two and last two so each X[i] tries to predict Y[i+2] (i've used i+2 and not to i+1 to force it to predict the future (O) )
